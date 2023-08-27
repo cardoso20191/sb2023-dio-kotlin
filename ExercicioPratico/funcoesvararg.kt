a27d819d1e64ee8505780fbfa09f311c3fc60328
cardoso20191 <cardoso2019@gmail.com>
2023-08-27 12:19:19 -0300

    feat: update Funcoes Vararg

--- ExercicioPratico/funcoesvararg.kt
+++ ExercicioPratico/funcoesvararg.kt
@@ -3,7 +3,7 @@ fun main() {
     fun printAll(vararg messages: String) {                            // 1
         for (m in messages) println(m)
     }
-    printAll("Hello", "Hallo", "Salut", "Hola", "你好")                 // 2
+    printAll("Hello", "Hallo", "Salut", "Hola", "Olá")                 // 2
     
 /*
     fun printAllWithPrefix(vararg messages: String, prefix: String) {  // 3
