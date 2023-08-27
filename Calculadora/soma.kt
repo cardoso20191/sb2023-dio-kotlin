8c25cfdf61a713bb3056abeea589c9b269cbb5ad
cardoso20191 <cardoso2019@gmail.com>
2023-08-27 10:56:19 -0300

    feat: update Hello-World

---
+++ Calculadora/soma.kt
@@ -0,0 +1,8 @@
+package calc.sb2023diokotlin.cardoso20191
+
+fun main() {
+    val nums: IntArray = intArrayOf(10, 10, 10, 10, 10)
+ 
+    val sum = nums.sum()
+    println("O Resultado da soma dos números é: $sum")
+}
