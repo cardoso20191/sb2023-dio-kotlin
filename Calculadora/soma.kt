8f96b948f2d4b52f522225a72368b846ea3b7b7a
Eduardo Reis <62727637+cardoso20191@users.noreply.github.com>
2023-08-27 11:38:43 -0300

    delete

--- Calculadora/soma.kt
+++
@@ -1,8 +0,0 @@
-package calc.sb2023diokotlin.cardoso20191
-
-fun main() {
-    val nums: IntArray = intArrayOf(10, 10, 10, 10, 10)
- 
-    val sum = nums.sum()
-    println("O Resultado da soma dos números é: $sum")
-}
