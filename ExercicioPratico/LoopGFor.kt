c4f6507ecc73bef07dcc00ccf3cb255f8e8a1f67
cardoso20191 <cardoso2019@gmail.com>
2023-09-07 11:54:50 -0300

    Feat: Loop For

---
+++ ExercicioPratico/LoopGFor.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val cakes = listOf("Cenoura", "Queijo", "chocolate")
+    
+    for (cake in cakes) {                               // 1
+        println("Yummy, it's a $cake cake!")
+    }
+
+
+}
